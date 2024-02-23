3ef30f4014fbbddb401ecf69326ca905039683ba
Leonel Ledesma <leonelledesma.dev@outlook.com>
2024-02-23 15:30:20 -0300

    Adición de la funcionalidad para desencriptar un texto

--- index.js
+++ index.js
@@ -28,3 +28,30 @@ function encriptar() {
 
 
 }
+
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let persona = document.getElementById("persona");
+
+
+    let textoCifrado = texto
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+
+    if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        persona.src = "./img/desencriptado.jpg";
+    } else {
+        persona.src = "./img/muñeco.png";
+        tituloMensaje.textContent = "Ningún mensaje fue encontrado"
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar"
+        alert("Debes ingresar algun texto")
+    }
+}
